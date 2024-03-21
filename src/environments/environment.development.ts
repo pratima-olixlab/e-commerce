e472d883152756354dc01d4ac75013917f53d7e8
pratima-olixlab <159017898+pratima-olixlab@users.noreply.github.com>
2024-03-21 14:34:18 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,14 +1,17 @@
 export const environment ={
-    production: true,
-    firebase: {
-      authDomain: 'e-commerce-2c30f.firebaseapp.com',
-      databaseURL: 'https://e-commerce-2c30f-default-rtdb.firebaseio.com',
-      projectId: 'e-commerce-2c30f',
-      storageBucket: 'e-commerce-2c30f.appspot.com',
-      messagingSenderId: '631672209169',
-      appId: '1:631672209169:web:587290bab48121e3c656b0',
-      measurementId: 'G-WN6PRSMWP1',
-    },
-  apiUrl: 'https://buy.stripe.com/test_00g6p02RY7GN41O7ss'
-
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAiIBCJee1atMl9T3UG4tG2DdvqBDowQ1c',
+    authDomain: 'e-commerce-2c30f.firebaseapp.com',
+    databaseURL: 'https://e-commerce-2c30f-default-rtdb.firebaseio.com',
+    projectId: 'e-commerce-2c30f',
+    storageBucket: 'e-commerce-2c30f.appspot.com',
+    messagingSenderId: '631672209169',
+    appId: '1:631672209169:web:587290bab48121e3c656b0',
+    measurementId: 'G-WN6PRSMWP1',
+  },
+  stripe: {
+    publicKey: 'pk_test_51OQMS8SBmrKIRoJWhVgg6myQKYC6OSEFOCcVQLT3TMBs7LWV30GvB9MZkKYAKBOzdnLQQTta3ZlQYTIcrJmfqNbs00NlzjPYwh'
+  },
+apiUrl: 'https://buy.stripe.com/test_00g6p02RY7GN41O7ss'
 }
